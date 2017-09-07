6aad6f7969f04ba4b8a6cd35f0f04ca69b5d8138
Boris Parak <parak@cesnet.cz>
2017-09-07 19:54:29 +0200

    Improved Rack::Attack logging and responses

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -3,11 +3,33 @@ module Rack
   class Attack
     throttle('req/ip', limit: 360, period: 1.minute, &:ip)
 
-    blocklist('auth-less annoyances') do |req|
+    blocklist('req/no-token') do |req|
       Allow2Ban.filter(req.ip, maxretry: 120, findtime: 1.minute, bantime: 24.hours) do
         req.env[Tokenator::TOKEN_HEADER_KEY].blank?
       end
     end
+  end
+end
+
+Rack::Attack.throttled_response = lambda do |env|
+  now = Time.current
+  match_data = env['rack.attack.match_data']
+
+  headers = {
+    'X-RateLimit-Limit' => match_data[:limit],
+    'X-RateLimit-Remaining' => 0,
+    'X-RateLimit-Reset' => now + (match_data[:period] - now.to_i % match_data[:period])
+  }
+
+  [429, headers, ['Too Many Requests']]
+end
+
+ActiveSupport::Notifications.subscribe('rack.attack') do |_, _, _, _, req|
+  case req.env['rack.attack.matched']
+  when 'req/ip'
+    Rails.logger.warn "[Rack::Attack] Throttling #{req.ip} after #{req.env['rack.attack.match_data'][:count]} requests"
+  when 'req/no-token'
+    Rails.logger.warn "[Rack::Attack] Blocking #{req.ip} after too many attempts without authorization"
   end
 end
 
