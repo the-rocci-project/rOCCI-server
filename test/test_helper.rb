eb80c705027d3704e7df6ae2530f11f9024bfee9
Boris Parak <parak@cesnet.cz>
2017-09-15 22:54:38 +0200

    Setting constants used in tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,18 @@
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
-module ActiveSupport
-  class TestCase
-    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-    fixtures :all
+module HelpfulTestConstants
+  TOKEN = 'am9obm55Om9wZW5uZWJ1bGEK'.freeze
 
-    # Add more helper methods to be used by all tests here...
-  end
+  LOCATION_FORMATS = %w[text/uri-list].freeze
+  LEGACY_FORMATS = %w[text/plain text/occi].freeze
+  FULL_FORMATS = %w[application/json application/occi+json].freeze
+  FORMATS = [LEGACY_FORMATS, FULL_FORMATS].flatten.freeze
+  ALL_FORMATS = [LOCATION_FORMATS, FORMATS].flatten.freeze
+
+  ENTITIES = %w[
+    compute network storage ipreservation securitygroup
+    networkinterface storagelink securitygrouplink
+  ].freeze
+  MULTI_ENTITIES = %w[entity resource link].freeze
 end
