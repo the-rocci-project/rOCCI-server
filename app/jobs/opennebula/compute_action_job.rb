c068dbd689d09be4b40d512101ef47c334e3dda2
Boris Parak <parak@cesnet.cz>
2017-09-28 17:16:14 +0200

    [ONe] Compute save w/ clean-up and attr removal

--- app/jobs/opennebula/compute_action_job.rb
+++ app/jobs/opennebula/compute_action_job.rb
@@ -5,8 +5,27 @@ module Opennebula
     INACTIVE_ACTIONS = %w[start save].freeze
     ACTIONS = [ACTIVE_ACTIONS, INACTIVE_ACTIONS].flatten.freeze
 
+    # Template/image attributes to remove after save
+    PURGE_ATTRIBUTES = %w[TEMPLATE/CLOUDKEEPER_ID TEMPLATE/CLOUDKEEPER_EXPIRED].freeze
+
     # :nodoc:
     ELM_NAME = 'virtual_machine'.freeze
+
+    protected
+
+    # :nodoc:
+    def job_cleanup!
+      super
+      return unless @_cleanup_templates
+      @_cleanup_templates.each do |tpl|
+        logger.warn "Cleaning up TEMPLATE##{tpl} after save action failed"
+        begin
+          handle { template(tpl).delete(true) }
+        rescue StandardError => ex
+          logger.error "Failed to clean up TEMPLATE##{tpl}: #{ex}"
+        end
+      end
+    end
 
     private
 
@@ -33,7 +52,33 @@ module Opennebula
     # :nodoc:
     def action_save(vm, attributes)
       new_name = attributes['name'].present? ? attributes['name'] : "saved-compute-#{vm['ID']}-#{Time.now.utc.to_i}"
-      vm.save_as_template new_name, true
+      rc = vm.save_as_template new_name, true
+      @_cleanup_templates = [rc] unless ::OpenNebula.is_error?(rc)
+      rc
+    end
+
+    # :nodoc:
+    def action_save_post(template_id)
+      tpl = template(template_id)
+      purge_disks_attributes! tpl
+      purge_attributes! tpl
+    end
+
+    # :nodoc:
+    def purge_disks_attributes!(template)
+      template.each_xpath('TEMPLATE/DISK') do |disk|
+        if disk['IMAGE_ID'].present?
+          purge_attributes! image(disk['IMAGE_ID'])
+        elsif disk['IMAGE'].present?
+          purge_attributes! image_by_name(disk['IMAGE'])
+        end
+      end
+    end
+
+    # :nodoc:
+    def purge_attributes!(obj)
+      PURGE_ATTRIBUTES.each { |attrib| obj.delete_element(attrib) }
+      handle { obj.update(obj.template_str) }
     end
   end
 end
--- app/jobs/opennebula/opennebula_job.rb
+++ app/jobs/opennebula/opennebula_job.rb
@@ -10,18 +10,22 @@ module Opennebula
 
     rescue_from(Errors::JobError) do |ex|
       logger.error "Delayed job failed: #{ex}"
+      job_cleanup!
     end
 
     rescue_from(Errors::Backend::EntityTimeoutError) do |_ex|
       logger.error "Timed out while waiting for job completion [#{DEFAULT_TIMEOUT}s]"
+      job_cleanup!
     end
 
     rescue_from(Errors::Backend::EntityRetrievalError) do |ex|
       logger.error "Failed to get instance state when waiting: #{ex}"
+      job_cleanup!
     end
 
     rescue_from(Errors::Backend::RemoteError) do |ex|
       logger.fatal "Failed during transition: #{ex}"
+      job_cleanup!
     end
 
     # Performs job in OpenNebula with given arguments.
@@ -33,26 +37,47 @@ module Opennebula
       @client = ::OpenNebula::Client.new(secret, endpoint)
     end
 
+    protected
+
+    # :nodoc:
+    def job_cleanup!; end
+
     private
 
     # :nodoc:
     def virtual_machine(identifier, state = nil)
-      virtual_machine = ::OpenNebula::VirtualMachine.new_with_id(identifier, @client)
-
-      if state
-        Backends.const_get(HELPER_NS)::Waiter.wait_until(virtual_machine, state, DEFAULT_TIMEOUT, :state_str)
-      else
-        handle { virtual_machine.info }
-      end
-
+      virtual_machine = oneject('virtual_machine', identifier)
+      Backends.const_get(HELPER_NS)::Waiter.wait_until(virtual_machine, state, DEFAULT_TIMEOUT, :state_str) if state
       virtual_machine
     end
 
     # :nodoc:
     def image(identifier, _state = nil)
-      image = ::OpenNebula::Image.new_with_id(identifier, @client)
-      handle { image.info }
-      image
+      oneject 'image', identifier
+    end
+
+    # :nodoc:
+    def image_by_name(name, _state = nil)
+      oneject_by_name 'image', name
+    end
+
+    # :nodoc:
+    def template(identifier, _state = nil)
+      oneject 'template', identifier
+    end
+
+    # :nodoc:
+    def oneject(type, identifier)
+      obj = ::OpenNebula.const_get(type.classify).new_with_id(identifier, @client)
+      handle { obj.info }
+      obj
+    end
+
+    # :nodoc:
+    def oneject_by_name(type, name)
+      obj = ::OpenNebula.const_get("#{type.classify}Pool").new(@client)
+      handle { obj.info_mine }
+      obj.detect { |elm| elm['NAME'] == name }
     end
 
     # :nodoc:
