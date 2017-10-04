da94f9d0589259c6767e295827f3fa9f5d5f4b6e
Boris Parak <parak@cesnet.cz>
2017-10-04 11:38:34 +0200

    Version bump to v2.0.0

--- config/version.rb
+++ config/version.rb
@@ -1,4 +1,17 @@
 module ROCCIServer
-  VERSION = '2.0.0.beta.2'.freeze
+  MAJOR_VERSION = 2                # Major update constant
+  MINOR_VERSION = 0                # Minor update constant
+  PATCH_VERSION = 0                # Patch/Fix version constant
+  STAGE_VERSION = nil              # use `nil` for production releases
+
+  unless defined?(::ROCCIServer::VERSION)
+    VERSION = [
+      MAJOR_VERSION,
+      MINOR_VERSION,
+      PATCH_VERSION,
+      STAGE_VERSION
+    ].compact.join('.').freeze
+  end
+
   ROCCI_VERSION = ::Occi::Core::VERSION if defined?(::Occi::Core::VERSION)
 end
